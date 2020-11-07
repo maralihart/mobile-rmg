bedf9466cb207f5791c1e6a7a1f78817e7225c90
Mara Hart <maralihart@gmail.com>
2020-11-07 18:07:14 -0500

    Added random messages

--- App.js
+++ App.js
@@ -2,13 +2,43 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+    };
+  }
+  render() {
+    if(message == "") {
+      return (
+        <View style={styles.container}>
+          <Button onPress={this.generateMessage} title="Generate Message" />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Button onPress={this.generateMessage} title="Change Message" />
+          <Text>{{this.state.message}}</Text>
+        </View>
+      )
+    }
+    
+  }
+}
+
+generateMessage() {
+  var messages = [
+    "Congratulations on hacking!",
+    "You're such a diligent programmer!",
+    "You've grown so much",
+    "The progress you've made is phenomenal"
+  ];
+
+  var random = Math.floor(Math.random()*(messages.length - 1));
+  var mess = messages[random];
+  this.setState({message: mess});
 }
 
 const styles = StyleSheet.create({
